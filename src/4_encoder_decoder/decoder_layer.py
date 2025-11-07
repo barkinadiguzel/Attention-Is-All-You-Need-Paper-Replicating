e00c3a1fbb679910120e95e6daba930fde1897a7
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-11-07 21:13:31 +0300

    Create decoder_layer.py

---
+++ src/4_encoder_decoder/decoder_layer.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+from src.attention.multi_head_attention import MultiHeadAttention
+from src.feed_forward.positionwise_ffn import PositionwiseFeedForward
+
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout=0.1):
+        super().__init__()
+        self.self_attn = MultiHeadAttention(d_model=d_model, h=num_heads, dropout=dropout)
+        self.enc_dec_attn = MultiHeadAttention(d_model=d_model, h=num_heads, dropout=dropout)
+        self.ffn = PositionwiseFeedForward(d_model=d_model, d_ff=d_ff, dropout=dropout)
+        self.norm1 = nn.LayerNorm(d_model, eps=1e-6)
+        self.norm2 = nn.LayerNorm(d_model, eps=1e-6)
+        self.norm3 = nn.LayerNorm(d_model, eps=1e-6)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
+
+    def forward(self, x, enc_output, tgt_mask=None, memory_mask=None):
+        _x = x
+        attn_output, _ = self.self_attn(x, x, x, mask=tgt_mask)
+        x = _x + self.dropout1(attn_output)
+        x = self.norm1(x)
+        _x = x
+        attn_output, _ = self.enc_dec_attn(x, enc_output, enc_output, mask=memory_mask)
+        x = _x + self.dropout2(attn_output)
+        x = self.norm2(x)
+        _x = x
+        ffn_output = self.ffn(x)
+        x = _x + self.dropout3(ffn_output)
+        x = self.norm3(x)
+        return x

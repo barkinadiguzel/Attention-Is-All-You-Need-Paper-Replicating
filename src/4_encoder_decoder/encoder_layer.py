7483fb5fb1f5982ee0d45861d2efb467d28bca8c
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-11-07 21:02:55 +0300

    Create encoder_layer.py

---
+++ src/4_encoder_decoder/encoder_layer.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+from src.attention import MultiHeadAttention
+from src.feed_forward import PositionwiseFeedForward
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout=0.1):
+        super().__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.ffn = PositionwiseFeedForward(d_model, d_ff)
+
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        attn_output = self.self_attn(x, x, x, mask)
+        x = x + self.dropout1(attn_output)
+        x = self.norm1(x)
+
+        ffn_output = self.ffn(x)
+        x = x + self.dropout2(ffn_output)
+        x = self.norm2(x)
+
+        return x
